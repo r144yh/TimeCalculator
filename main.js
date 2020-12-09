29e7215d3113e9fef88730aecc7fb035be9c4f48
r144yh <mrakur@yandex.ru>
2020-12-10 01:36:15 +0300

    init: start time project

---
+++ main.js
@@ -0,0 +1,32 @@
+const timeForm = document.getElementById('timeForm');
+const optimisticNumber = document.getElementById('optimisticNumber');
+const possibleNumber = document.getElementById('possibleNumber');
+const pessimisticNumber = document.getElementById('pessimisticNumber');
+const formBtn = document.getElementById('formBtn');
+
+let optimisticValue = null,
+    possibleValue = null,
+    pessimisticValue = null,
+    formulaAns = null;
+
+formBtn.addEventListener('click', function(event) {
+  optimisticValue = optimisticNumber.value;
+  possibleValue = possibleNumber.value;
+  pessimisticValue = pessimisticNumber.value;
+
+  formulaAns = (Number(optimisticValue) + (4 * possibleValue) +
+      Number(pessimisticValue)) / 6;
+  formulaAns = Math.round(formulaAns);
+
+  document.body.removeChild(timeForm);
+  const answerArea = document.createElement('div');
+  answerArea.classList.add('form');
+  document.body.append(answerArea);
+
+  const answer = document.createElement('div');
+  answer.classList.add('input__area');
+  answerArea.append(answer);
+  formulaAns > 1 ?
+      answer.innerHTML = `${formulaAns} days` :
+      answer.innerHTML = `${formulaAns} day`;
+});
